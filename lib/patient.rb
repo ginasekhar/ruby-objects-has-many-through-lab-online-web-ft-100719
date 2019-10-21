0b928b486f7a9255dfcdea964c2126b8cdb0e745
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-21 17:19:34 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -18,6 +18,17 @@ class Patient
     Appointment.new(date, self, doctor)
   end #new_appointment
   
+  def doctors
+    all_my_doctors = []
+    Appointment.all.select do |appt| 
+      if appt.patient == self
+        all_my_doctors << appt.doctor
+      end #if
+    end# do
+    all_my_doctors
+    
+  end #doctors
+  
   def self.all
     @@all
   end #all 
